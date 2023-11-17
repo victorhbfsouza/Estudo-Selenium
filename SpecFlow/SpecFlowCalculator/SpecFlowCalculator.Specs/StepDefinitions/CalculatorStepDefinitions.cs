510e9c52fea532cf2d5e56e397791fd97a3504ee
Victor Hugo Souza <127329074+victorhbfsouza@users.noreply.github.com>
2023-11-17 10:37:27 -0300

    :sparkles: Add Subtracted

--- SpecFlow/SpecFlowCalculator/SpecFlowCalculator.Specs/StepDefinitions/CalculatorStepDefinitions.cs
+++ SpecFlow/SpecFlowCalculator/SpecFlowCalculator.Specs/StepDefinitions/CalculatorStepDefinitions.cs
@@ -36,6 +36,13 @@ namespace SpecFlowCalculator.Specs.Steps
             _result = _calculator.Add();
         }
 
+        [When(@"the two numbers are subtracted")]
+        public void WhenTheTwoNumbersAreSubtracted()
+        {
+            _result = _calculator.Subtract();
+        }
+
+
         [Then("the result should be (.*)")]
         public void ThenTheResultShouldBe(int result)
         {
